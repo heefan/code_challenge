2aabd14ad050ed953fcd5b501f7db8af9a0a9d62
heefan <heefan@gmail.com>
2015-04-29 23:58:08 +0800

    add easy 02: First Factorial

---
+++ CodeByte/easy_02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int FirstFactorial(int num) {
+
+  // code goes here
+  return num;
+
+}
+
+int main() {
+
+  // keep this function call here
+  cout << FirstFactorial(gets(stdin));
+  return 0;
+
+}
